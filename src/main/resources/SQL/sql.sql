b0d980885e2362dfc73b2e2dee7d0eacc93d1266
cuc2808 <cucogco@gmail.com>
2026-05-11 12:58:30 +0200

    Added a users table to the SQL

--- src/main/resources/SQL/sql.sql
+++ src/main/resources/SQL/sql.sql
@@ -5,3 +5,19 @@ CREATE DATABASE "CarportDB"
     LOCALE_PROVIDER = 'libc'
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id bigserial,
+    email character varying,
+    password character varying,
+    first_name character varying,
+    last_name character varying,
+    phone_number character varying,
+    is_admin boolean DEFAULT false,
+    PRIMARY KEY (user_id)
+    );
+END;
