00df7dbac68e480d2b3866826273406656e07049
NicolaiFaellesskov <nicolainoah@gmail.com>
2026-05-13 13:52:49 +0200

    Update on sql Some Carport Datatypes changed to numeric Status have CHECK now And some commas have been replaced, so it works

--- src/main/resources/SQL/sql.sql
+++ src/main/resources/SQL/sql.sql
@@ -1,12 +1,13 @@
 BEGIN;
+
 CREATE TABLE IF NOT EXISTS public.users
 (
     user_id bigserial NOT NULL,
-    email character varying COLLATE pg_catalog."default",
-    password character varying COLLATE pg_catalog."default",
-    first_name character varying COLLATE pg_catalog."default",
-    last_name character varying COLLATE pg_catalog."default",
-    phone_number character varying COLLATE pg_catalog."default",
+    email varchar NOT NULL UNIQUE,
+    password character varying NOT NULL,
+    first_name character varying NOT NULL,
+    last_name character varying NOT NULL,
+    phone_number character varying NOT NULL,
     is_admin boolean DEFAULT false,
     CONSTRAINT users_pkey PRIMARY KEY (user_id)
     );
@@ -17,23 +18,26 @@ CREATE TABLE IF NOT EXISTS public.carports
     amount_of_cars bigint NOT NULL DEFAULT 1,
     length bigint NOT NULL,
     width bigint NOT NULL,
-    has_shed boolean,
+    has_shed boolean NOT NULL DEFAULT false,
     shed_width bigint,
     shed_length bigint,
-    has_gutter boolean,
+    has_gutter boolean NOT NULL DEFAULT false,
     user_id bigint NOT NULL,
+
     CONSTRAINT carport_pkey PRIMARY KEY (carport_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    CONSTRAINT fk_carport_user
+    FOREIGN KEY (user_id)
+    REFERENCES public.users(user_id)
     );
 
 CREATE TABLE IF NOT EXISTS public.inquiries
 (
     inquiry_id bigserial NOT NULL,
-    status character varying NOT NULL,
+    status character varying NOT NULL CHECK (status IN ('pending', 'offered', 'accepted', 'rejected')),
     user_id bigint NOT NULL,
     carport_id bigint NOT NULL,
     date date NOT NULL,
-    price bigint,
+    price numeric,
     PRIMARY KEY (inquiry_id),
 
     CONSTRAINT fk_inquiry_user
@@ -49,6 +53,7 @@ CREATE TABLE IF NOT EXISTS public.part
 (
     part_id bigserial NOT NULL,
     part_name character varying NOT NULL,
-    part_price bigint
-);
+    part_price numeric,
+    PRIMARY KEY (part_id)
+    );
 END;
