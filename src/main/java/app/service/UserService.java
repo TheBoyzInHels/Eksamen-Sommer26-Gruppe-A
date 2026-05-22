5cff18096347490ee285d84a7446466d4842fa7f
Peter Mærsk <petersondergaard1@gmail.com>
2026-05-22 11:45:32 +0200

    Author: Peter Deleted a wandering generatePartsList inside UserService. User is refactored to and has no code that's unused now.

--- src/main/java/app/service/UserService.java
+++ src/main/java/app/service/UserService.java
@@ -1,23 +1,12 @@
 package app.service;
 
-import app.entities.Carport;
 import app.entities.User;
-import app.exceptions.DatabaseException;
-import app.persistence.ConnectionPool;
 import io.javalin.http.Context;
 
 public class UserService {
 
-    public boolean existingUser(User user) {
-        return user != null;
-    }
-
     public static User currentUser(Context ctx) {
         return ctx.sessionAttribute("currentUser");
-    }
-    public static void generatePartsList(Carport carport){
-
-
     }
 }
 
